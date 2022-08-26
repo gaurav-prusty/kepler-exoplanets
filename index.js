7158319ca67812e9ce29309b9188099233e2e83c
gaurav-prusty <prustycolab@gmail.com>
2022-08-26 17:32:58 +0530

    feat: habitable planets

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function ishabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;            //check for stellar flux as well
+}
 
 
 fs.createReadStream('kepler_data.csv')
-    .pipe(parse({
+    .pipe(parse({                               //parse return a javascript object for better readability
         comment: '#',
         columns: true
-    }))
+    }))                                                 
     .on('data', (data) => {
-        results.push(data);
+        if (ishabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(results);
-        console.log('done');
+        console.log(habitablePlanets);
+        console.log(`Total habitable planets: ${habitablePlanets.length}`);
+        console.log('DONE!!');
     })
